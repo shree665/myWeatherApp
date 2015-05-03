a7a1df6e8923a5da579dd6f1cb8e7d0c7281469c
Vivek Subedi <shree665@gmail.com>
2015-05-02 22:09:47 -0400

    clean up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,42 @@
 angular.module('myWeatherApp.controllers', [])
+    .controller('CurrentCtrl', function($scope, $http, $ionicLoading, $timeout, $location) {
+            console.log("You are on current Controller");
+            var url = "https://api.forecast.io/forecast/0ef17b0eba286f81dbde403ca9456f32/38.854681,-77.165222" + "?callback=JSON_CALLBACK";
+            $http.get(url).success(function(data) {
+                    $scope.weatherData = data;
+                    console.log($scope.weatherData);
+                }).
+                error(function(data, status) {
+                    console.log("Error has occurred: " + status);
+                    console.log(data);
+                });
+        })
 
-.controller('CurrentCtrl', function($scope, $http, $ionicLoading, $timeout, $location) {
-        console.log("You are on current Controller");
-        var url = "https://api.forecast.io/forecast/0ef17b0eba286f81dbde403ca9456f32/38.854681,-77.165222" + "?callback=JSON_CALLBACK";
-        $http.get(url)
-            .success(function(data) {
-                $scope.weatherData = data;
-                console.log($scope.weatherData);
-            }).
-            error(function(data, status) {
-                console.log("Error has occurred: " + status);
-                console.log(data);
-            });
+    .controller('FavoriteCtrl', function($scope, Chats) {
+            console.log("You are on favorite Controller");
+            $scope.chats = Chats.all();
+            $scope.remove = function(chat) {
+            Chats.remove(chat);
+            }
     })
 
-.controller('FavoriteCtrl', function($scope, Chats) {
-        console.log("You are on favorite Controller");
-        $scope.chats = Chats.all();
-        $scope.remove = function(chat) {
-        Chats.remove(chat);
-        }
-})
+    .controller('CityDetailCtrl', function($scope, $stateParams, Chats) {
+      $scope.chat = Chats.get($stateParams.chatId);
+    });
 
-.controller('CityDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-});
+
+/*
+ angular.module('myModule')
+ .config(function ( $httpProvider) {
+ delete $httpProvider.defaults.headers.common['X-Requested-With'];
+ }).factory('featuresData', function ($http) {
+ return{
+ doCrossDomainGet: function() {
+ return $http({
+ url:'http://other.domain.com',
+ method: 'GET'
+ })
+ }
+ }
+ });
+ */
